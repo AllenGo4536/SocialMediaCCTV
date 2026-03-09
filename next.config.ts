dc6a16292f60e59f60b8feb7f58027e895c43254
AllenGo4536 <xutielun@gmail.com>
2026-03-09 13:08:41 +0800

    feat: Configure Next.js image optimization to allow local patterns for proxy and general paths.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    localPatterns: [
+      {
+        pathname: '/api/proxy/image',
+      },
+      {
+        pathname: '/**',
+        search: '',
+      },
+    ],
     remotePatterns: [
       {
         protocol: 'https',
